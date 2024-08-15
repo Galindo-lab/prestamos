3028f7ec2273561996098e157b0b7e7a40970184
galindo <egalindo54@uabc.edu.mx>
2024-08-15 12:34:47 -0700

    login agregado

--- prestamos/urls.py
+++ prestamos/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, reverse_lazy
 
 from prestamos.views import OrderAuthorize
 from prestamos.views import ReportCreateView
@@ -6,7 +7,18 @@ from prestamos.views import OrderCreateView
 from prestamos.views import OrderDetailView
 from prestamos.views import OrderListView
 
+
 urlpatterns = [
+    # login
+    path(route='login/', name='login', view=LoginView.as_view(
+        template_name='login.html'
+    )),
+
+    # logout
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('', OrderListView.as_view(), name=""),
+
     path('orders/create/', OrderCreateView.as_view(), name='order_create'),
     path('orders/', OrderListView.as_view(), name='order_list'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
