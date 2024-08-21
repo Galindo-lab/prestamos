07135f1d5ca0729e7cae240ef24b9ae40f7516bc
galindo <egalindo54@uabc.edu.mx>
2024-08-21 10:35:09 -0700

    sistema de tema cargado

--- prestamos/urls.py
+++ prestamos/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, reverse_lazy
 
-from prestamos.views import OrderAuthorize, SettingsView
+from prestamos.views import OrderAuthorize, SettingsView, OrderHistoryListView
 from prestamos.views import ReportCreateView
 from prestamos.views import OrderCreateView
 from prestamos.views import OrderDetailView
-from prestamos.views import OrderListView
+# from prestamos.views import OrderListView
 
 
 urlpatterns = [
@@ -21,7 +21,7 @@ urlpatterns = [
     # logout
     path('logout/', LogoutView.as_view(), name='logout'),
     path('orders/create/', OrderCreateView.as_view(), name='order_create'),
-    path('orders/', OrderListView.as_view(), name='order_list'),
+    path('orders/history', OrderHistoryListView.as_view(), name='order_history_list'),
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order_detail'),
     path('orders/authorize/<int:pk>/', OrderAuthorize.as_view(), name='order_aprove'),
     path('orders/report/<int:pk>/', ReportCreateView.as_view(), name='order_report'),
--- prestamos/views.py
+++ prestamos/views.py
@@ -16,7 +16,16 @@ from .forms import OrderForm, AuthorizeForm, OrderItemFormSet, ReporteForm
 from .models import Order, Report, Item
 
 
-class SettingsView(TemplateView):
+"""class OrderListView(LoginRequiredMixin, ListView):
+    model = Order
+    template_name = 'order_list.html'
+    context_object_name = 'orders'
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)"""
+
+
+class SettingsView(LoginRequiredMixin, TemplateView):
     template_name = "settings.html"
 
 
@@ -95,7 +104,7 @@ class OrderCreateView(LoginRequiredMixin, View):
         })
 
 
-class OrderListView(LoginRequiredMixin, ListView):
+class OrderHistoryListView(LoginRequiredMixin, ListView):
     model = Order
     template_name = 'order_list.html'
     context_object_name = 'orders'
