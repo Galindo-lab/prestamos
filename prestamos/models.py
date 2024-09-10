bfeab77843b609d5ffb5c0ce0b1f41488ef07c2b
galindo <egalindo54@uabc.edu.mx>
2024-09-10 01:42:45 -0700

    nombre para categorias debe ser unico

--- prestamos/models.py
+++ prestamos/models.py
@@ -21,7 +21,7 @@ class Category(models.Model):
         verbose_name = "Categoría"
         verbose_name_plural = "Categorías"
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
